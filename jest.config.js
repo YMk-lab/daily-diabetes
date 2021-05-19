7f1772becd1128f9f129b94f3fbcc8c7de1a1474
Yura Moryliak <y.moryliak@crosssoft.de>
2021-05-19 22:28:18 +0300

    impl-ionic-app - install nxtend - generate starter application

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '<rootDir>/apps/client',
     '<rootDir>/apps/backend',
     '<rootDir>/libs/material-shared',
-    '<rootDir>/libs/shared-data'
+    '<rootDir>/libs/shared-data',
+    '<rootDir>/apps/mobile',
   ],
 };
